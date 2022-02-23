e03d3be8b79238025a120fb6d3674b7a40f6a372
Leidy <lady-2110@hotmail.com>
2022-02-22 20:53:44 -0500

    continuación del juego, se agrega el evento click para seleccionar la imagen, que cuente la cantidad de aciertos y cambien la imagen por bien

--- funciones.js
+++ funciones.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const imagenes = [
-        { img: '/imagenes/caballo.png', name: 'caballo' },
-        { img: '/imagenes/bien.png', name: 'bien' },
-        { img: '/imagenes/cerdo.png', name: 'cerdo' },
-        { img: '/imagenes/elefante.png', name: 'elefante' },
-        { img: '/imagenes/interroga.png', name: 'interrogante' },
-        { img: '/imagenes/leon.png', name: 'leon' },
-        { img: '/imagenes/mono.png', name: 'mono' },
-        { img: '/imagenes/oso.png.crdownload', name: 'oso' },
-        { img: '/imagenes/tigre.png', name: 'tigre' },
-        { img: '/imagenes/cebra.png', name: 'cebra' },
-        { img: '/imagenes/loro.png', name: 'loro' },
-        { img: '/imagenes/perro.png', name: 'perro' },
+        { img: '/imagenes/reno.png', name: 'reno' },
+        { img: '/imagenes/lufi.png', name: 'lufi' },
+        { img: '/imagenes/zoro.png', name: 'zoro' },
+        { img: '/imagenes/nami.png', name: 'nami' },
+        { img: '/imagenes/ace.png', name: 'ace' },
+        { img: '/imagenes/usop.png', name: 'usop' },
+        { img: '/imagenes/reno.png', name: 'reno' },
+        { img: '/imagenes/lufi.png', name: 'lufi' },
+        { img: '/imagenes/zoro.png', name: 'zoro' },
+        { img: '/imagenes/nami.png', name: 'nami' },
+        { img: '/imagenes/ace.png', name: 'ace' },
+        { img: '/imagenes/usop.png', name: 'usop' },
     ]
 
     //tablero html
@@ -20,7 +20,13 @@ document.addEventListener('DOMContentLoaded', () => {
     let imgElegida = [];
     //Arreglo para guardar el id ID 
     let imgElegidaID = [];
+    //aciertos
+    let aciertos = document.querySelector(".conteo");
+    let conteo = [];
 
+    //funcion patra colocar las imagenes de pregunta en el tablero
+    //funcion para colocar aleatorias las imagenes
+    imagenes.sort( ()=>0.5 - Math.random);
     //funcion para colocar las imagenes en el html
 
     const crearTablero = () => {
@@ -28,8 +34,9 @@ document.addEventListener('DOMContentLoaded', () => {
             
             let img = document.createElement('img')
             img.setAttribute('data-id', k);
-            img.setAttribute('src', '/imagenes/interroga.png');
+            img.setAttribute('src', '/imagenes/interroga.jpg');
             img.setAttribute('width', '200px');
+            img.setAttribute('class','a');
             tablero.appendChild(img);
             img.addEventListener("click",descubrirImagen);         
         }
@@ -38,11 +45,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //funcion para descurbrir una imagen del juego
     function descubrirImagen(){
-        let img = this.getAttribute("data-id");
-        imgElegida.push(imagenes[img].name);
-    alert(imagenes[img].name);
+        let imgClick = this.getAttribute("data-id");
+        imgElegida.push(imagenes[imgClick].name);
+        imgElegidaID.push(imgClick);
+        //alert(imagenes[img].name);
+        this.setAttribute("src",imagenes[imgClick].img);
+        if(imgElegida.length===2){
+            setTimeout(() => {
+                compararimagen()
+            }, 300);
+        }
     }
 
+   
+    //Función para comprar imagenes
+    function compararimagen(){
+    let todaslasImg = document.querySelectorAll("img");
+    let opcion1 = imgElegidaID[0];
+    let opcion2 = imgElegidaID[1];
+    if(imgElegida[0] === imgElegida[1]){
+        alert("Correcto!!!");
+        todaslasImg[opcion1].setAttribute("src","/imagenes/bien.png");
+        todaslasImg[opcion2].setAttribute("src","/imagenes/bien.png");
+        conteo.push(imgElegida);
+    }else{
+        alert("Sigue Intentanto!!!");
+        todaslasImg[opcion1].setAttribute("src","/imagenes/interroga.jpg");
+        todaslasImg[opcion2].setAttribute("src","/imagenes/interroga.jpg");
+    }
+    imgElegida=[];
+    imgElegidaID=[];
+    aciertos.textContent = conteo.length;
+
+    if(conteo.length===6){
+        aciertos.textContent = "Ganaste" 
+    }
+    }
     crearTablero();
 
 
